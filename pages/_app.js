2767d5cd795130b0bd0c19078522b7905b49aa88
bolub <abiol5202@gmail.com>
2021-03-26 19:48:45 +0100

    first iteration complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+// chakra
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+import { Fonts } from "../chakra/Fonts";
+// chakra
+import { customTheme } from "../chakra/theme";
+
+// global css
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Zane and Zuri Oyediji</title>
+      </Head>
+      <ChakraProvider theme={customTheme}>
+        <Fonts />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
